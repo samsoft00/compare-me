694e529cf32a9d8b24d652e08cf2e1c66ee6c006
samsoft00 <oyewoleabayomi@gmail.com>
2015-11-06 03:47:05 +0100

    Initial Commit

---
+++ tut_site/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def hello_world(request):
+		# return HttpResponse('Hello World')
+		return render(request, 'home.html')
